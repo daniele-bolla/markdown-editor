e4c3cb0b869d3a29818d13e0af40230c3c3874f3
bolladaniele <bolladaniele@gmail.com>
2024-07-16 15:03:50 +0100

    init vuetify and style project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <MarkdownEditor />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-centerfill-height mx-auto" max-width="1120px">
+      <div class="text-center">
+        <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+        <h1 class="text-h2 font-weight-bold">Content</h1>
+      </div>
+      <v-row>
+        <v-col cols="12">
+          <div class="my-0 border rounded">
+            <nav
+              class="d-flex justify-space-between text-light_grey mx-4 border-b-thin"
+            >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Image</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Video</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Description</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Quote</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Footnote</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Link</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Button</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >File</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >2 columns</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Table</a
+              >
+              <a
+                variant="plain"
+                class="text-none text-light_grey font-weight-regular text-decoration-none py-3"
+                href="#"
+                >Table row</a
+              >
+            </nav>
+            <v-textarea
+              aria-placeholder="Placeholder"
+              placeholder="Placeholder"
+              variant="solo"
+              hide-details="true"
+            ></v-textarea>
+          </div>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style>
+.w-full {
+  width: 100%;
+}
+.list-none {
+  list-style: none;
+}
+</style>
