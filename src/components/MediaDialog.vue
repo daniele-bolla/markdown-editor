ee531d7c23e476140c70af3a89f29b7c67068b16
bolladaniele <bolladaniele@gmail.com>
2024-07-17 11:49:38 +0100

    fake upload

--- src/components/MediaDialog.vue
+++ src/components/MediaDialog.vue
@@ -3,10 +3,14 @@
     <v-card max-width="600" :title="props.title">
       <v-list lines="one">
         <v-list-item>
-          <v-file-input label="File input" multiple></v-file-input>
+          <v-file-input
+            label="File input"
+            @input="onUpload"
+            multiple
+          ></v-file-input>
         </v-list-item>
         <v-list-item
-          v-for="media in props.media"
+          v-for="media in items"
           :key="media.id"
           :title="media.title"
           @click="selectMedia(media.id)"
@@ -53,13 +57,14 @@ const props = <Props>defineProps(["title", "media", "modelValue"]);
 const emit = <Emit>defineEmits(["insert", "update:modelValue"]);
 
 const selectedMedia = ref<Media | undefined>();
+const items = ref<Media[] | undefined>(props.media);
 
 function isSelected(id: string) {
   return selectedMedia.value && selectedMedia.value.id == id;
 }
 function selectMedia(id: string) {
-  if (props.media) {
-    selectedMedia.value = props.media.find((i) => i.id == id);
+  if (items.value) {
+    selectedMedia.value = items.value.find((i) => i.id == id);
   }
 }
 function onClose() {
@@ -70,5 +75,25 @@ function onInserMedia() {
     emit("insert", selectedMedia.value);
     onClose();
   }
+}
+function fakeCall(data: any, time: number = 1000): Promise<Media[]> {
+  let timer;
+  clearTimeout(timer);
+  return new Promise((resolve) => {
+    timer = setTimeout(() => {
+      resolve(data);
+    }, time);
+  });
+}
+const fakeResponse = [
+  {
+    id: "img_03",
+    title: "Waterfall",
+    url: "https://images.unsplash.com/photo-1721112796760-fe228d1e22a8?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+async function onUpload() {
+  const data = await fakeCall(fakeResponse);
+  items.value = items.value ? [...items.value, ...data] : data;
 }
 </script>
